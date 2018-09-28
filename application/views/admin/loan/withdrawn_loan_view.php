d573c678aa28146751c49c493d30b4fb47e1caf4
ebirulz <ebi.okpongu@gmail.com>
2018-09-28 23:23:14 +0100

    loan declined, disbursed,withdrawn, partial disbursed views

---
+++ application/views/admin/loan/withdrawn_loan_view.php
@@ -0,0 +1,74 @@
+
+    <div class="container-fluid animatedParent animateOnce">
+        <div class="tab-content my-3" id="v-pills-tabContent">
+            <div class="tab-pane animated fadeInUpShort show active" id="v-pills-all" role="tabpanel" aria-labelledby="v-pills-all-tab">
+                <div class="row my-3">
+                    <div class="col-md-12">
+                        <div class="card r-0 shadow">
+                            <div class="table-responsive">
+                                <form>
+                                    <table class="table table-striped table-hover r-0 data-tables" data-options='{ "paging": false; "searching":false}'>
+                                        <thead>
+                                        <tr class="no-b">
+                                            <th style="width: 30px">
+                                                <div class="custom-control custom-checkbox">
+                                                    <input type="checkbox" id="checkedAll" class="custom-control-input"><label
+                                                        class="custom-control-label" for="checkedAll"></label>
+                                                </div>
+                                            </th>
+                                            <th>APPLICANT NAME</th>
+
+                                            <th> <div class="d-none d-lg-block">LOAN ID</div></th>
+                                            <th> <div class="d-none d-lg-block">LOAN TYPE</div></th>
+                                            <th> <div class="d-none d-lg-block">REQUESTED</div></th>
+                                            <th> <div class="d-none d-lg-block">DISBURSED</div></th>
+                                            <th> <div class="d-none d-lg-block">STATUS</div></th>
+                                            <th></th>
+                                        </tr>
+                                        </thead>
+
+                                        <tbody>
+                                        <tr>
+                                            <td>
+                                                <div class="custom-control custom-checkbox">
+                                                    <input type="checkbox" class="custom-control-input checkSingle"
+                                                           id="user_id_32" required><label
+                                                        class="custom-control-label" for="user_id_1"></label>
+                                                </div>
+                                            </td>
+                                            <td>
+                                              <div class="d-flex">
+                                                  <div class="avatar avatar-md mr-3 mb-2 mt-1">
+                                                      <span class="avatar-letter avatar-letter-d  avatar-md circle"></span>
+                                                  </div>
+                                                  <div>
+                                                      <div>
+                                                          <strong>Ebimobowei Okpongu</strong>
+                                                      </div>
+                                                      <small> ebi.okpongu@gmail.com</small>
+                                                  </div>
+                                              </div>
+                                            </td>
+
+                                            <td> <div class="d-none d-lg-block">BYSHESLB/B1/101</div></td>
+                                            <td> <div class="d-none d-lg-block"><span class="r-3 badge badge-success ">Undergratuate</span></div></td>
+
+                                            <td> <div class="d-none d-lg-block">700,000.00</div></td>
+                                            <td> <div class="d-none d-lg-block">0.00</div></td>
+                                            <td> <div class="d-none d-lg-block"><span class="r-3 badge badge-success ">Withdrawn</span></div></td>
+                                            <td>
+                                                <a href="panel-page-profile.html"><i class="icon-eye mr-3"></i></a>
+                                                <a href="panel-page-profile.html"><i class="icon-pencil"></i></a>
+                                            </td>
+                                        </tr>
+
+                                        </tbody>
+                                    </table>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+		</div>
+	</div>
