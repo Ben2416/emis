7e17a930b45782e3355565ddee57db59355936a6
ebirulz <ebi.okpongu@gmail.com>
2018-09-29 01:21:48 +0100

    admin Loan controller

---
+++ application/controllers/admin/Loan.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Students extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+		$this->load->model('Loan_model');
+    }
+
+    public function index(){
+		$this->load->view('admin/header_view');
+		$this->load->view('admin/loan/all_loan_view');
+		$this->load->view('admin/footer_view');
+    }
+
+    public function approvedLoan(){
+		$this->load->view('admin/header_view');
+		$this->load->view('admin/loan/approved_loan_view');
+		$this->load->view('admin/footer_view');
+    }
+
+    public function declinedLoan(){
+		$this->load->view('admin/header_view');
+		$this->load->view('admin/loan/declined_loan_view');
+		$this->load->view('admin/footer_view');
+    }
+
+    public function disbursedLoan(){
+		$this->load->view('admin/header_view');
+		$this->load->view('admin/loan/disbursed_loan_view');
+		$this->load->view('admin/footer_view');
+    }
+
+    public function partialDisbursedLoan(){
+		$this->load->view('admin/header_view');
+		$this->load->view('admin/loan/partial_disbursed_loan_view');
+		$this->load->view('admin/footer_view');
+    }
+
+    public function pendingLoan(){
+		$this->load->view('admin/header_view');
+		$this->load->view('admin/loan/pending_loan_view');
+		$this->load->view('admin/footer_view');
+    }
+
+    public function withdrawnLoan(){
+		$this->load->view('admin/header_view');
+		$this->load->view('admin/loan/withdrawnLoan_loan_view');
+		$this->load->view('admin/footer_view');
+    }
+}
