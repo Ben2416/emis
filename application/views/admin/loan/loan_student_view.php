1cc611826143172183bffa0ff6a5ce682558ca4b
Ebirulz <ebi.okpongu@gmail.com>
2018-10-14 16:56:43 +0100

    Created the student loan page, modification to student view

--- application/views/admin/loan/loan_student_view.php
+++ application/views/admin/loan/loan_student_view.php
@@ -21,7 +21,15 @@
                               <i class="icon icon-home2"></i>Home
                           </a>
                       </li>
-
+                      <li>
+                          <a class="nav-link" id="v-pills-payments-tab" data-toggle="pill" href="#v-pills-payments" role="tab" aria-controls="v-pills-payments" aria-selected="false"><i class="icon icon-money-1"></i>Loans</a>
+                      </li>
+                    <!--  <li>
+                          <a class="nav-link" id="v-pills-timeline-tab" data-toggle="pill" href="#v-pills-timeline" role="tab" aria-controls="v-pills-timeline" aria-selected="false"><i class="icon icon-cog"></i>Loan Timeline</a>
+                      </li>
+                     <li>
+                          <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false"><i class="icon icon-cog"></i>Edit Profile</a>
+                      </li> -->
                   </ul>
               </div>
 
@@ -55,6 +63,40 @@
                        </div>
                        <div class="col-md-9">
 
+                           <div class="row">
+                               <div class="col-lg-4">
+                                   <div class="card r-3">
+                                       <div class="p-4">
+                                           <div class="float-right">
+                                               <span class="icon-award text-light-blue s-48"></span>
+                                           </div>
+                                           <div class="counter-title">Loans Applied</div>
+                                           <h5 class=""><?=$loans_applied?></h5>
+                                       </div>
+                                   </div>
+                               </div>
+                               <div class="col-lg-4">
+                                   <div class="card r-3">
+                                       <div class="p-4">
+                                           <div class="float-right"><span class="icon-stop-watch3 s-48"></span>
+                                           </div>
+                                           <div class="counter-title ">Total Amount</div>
+                                           <h5 class="">N <?=$loans_total->sum_total?></h5>
+                                       </div>
+                                   </div>
+                               </div>
+                               <div class="col-lg-4">
+                                   <div class="white card">
+                                       <div class="p-4">
+                                           <div class="float-right"><span class="icon-orders s-48"></span>
+                                           </div>
+                                           <div class="counter-title">Due Date</div>
+                                           <h5 class="">26 Sept 2019</h5>
+                                       </div>
+                                   </div>
+                               </div>
+                           </div>
+
                            <div class="row my-3">
                                <!-- bar charts group -->
                                <div class="col-md-12">
@@ -80,8 +122,8 @@
                                                            </ul>
                                                        </div>
                                                        <div class="align-self-center">
-                                                           <h5><a href="<?=base_url()?>admin/Students/acceptApplication/<?=$profile['user_id']?>" class="btn btn-xs btn-success">Accept Application</a>
-                                                           <a href="<?=base_url()?>admin/Students/rejectApplication/<?=$profile['user_id']?>" class="btn btn-xs btn-danger">Reject Application</a></h5>
+                                                           <h5><a href="<?=base_url()?>admin/#" class="btn btn-xs btn-success">Approve Loan</a>
+                                                           <a href="<?=base_url()?>admin/#" class="btn btn-xs btn-danger">Reject Loan</a></h5>
                                                        </div>
 
                                                    </div>
@@ -288,55 +330,9 @@
                                    </div>
                                                        </div>
                                                        <div class="tab-pane fade text-center p-5" id="w3-tab3" role="tabpanel" aria-labelledby="w3-tab3">
-                                                         <div class="row">
-                                                             <div class="col-md-4 col-xs-6 b-r"> <strong>Guarantor Name</strong>
-                                                                 <br>
-                                                                 <p class="text-muted">{title|First Name|last name}</p>
-                                                             </div>
-                                                             <div class="col-md-4 col-xs-6 b-r"> <strong>Phone Number</strong>
-                                                                 <br>
-                                                                 <p class="text-muted">{Phone Number}</p>
-                                                             </div>
-                                                             <div class="col-md-4 col-xs-6"> <strong>Alternative Phone</strong>
-                                                                 <br>
-                                                                 <p class="text-muted">{Alternative Phone}</p>
-                                                             </p></div>
-
-                                                         </div>
-
-                                                         <div class="row">
-                                                             <div class="col-md-4 col-xs-6 b-r"> <strong>Contact Address</strong>
-                                                                 <br>
-                                                                 <p class="text-muted">{Contact Address}</p>
-                                                             </div>
-                                                             <div class="col-md-4 col-xs-6 b-r"> <strong>State</strong>
-                                                                 <br>
-                                                                 <p class="text-muted">{state}</p>
-                                                             </div>
-                                                             <div class="col-md-4 col-xs-6"> <strong>Occupation</strong>
-                                                                 <br>
-                                                                 <p class="text-muted">{Occupation}</p>
-                                                             </p></div>
-
-                                                         </div>
-
-                                                         <div class="row">
-                                                             <div class="col-md-4 col-xs-6 b-r"> <strong>Employer</strong>
-                                                                 <br>
-                                                                 <p class="text-muted">{Employer}</p>
-                                                             </div>
-                                                             <div class="col-md-4 col-xs-6 b-r"> <strong>Position Held</strong>
-                                                                 <br>
-                                                                 <p class="text-muted">{Position Held}</p>
-                                                             </div>
-                                                             <div class="col-md-4 col-xs-6"> <strong>Relationship with Applicant</strong>
-                                                                 <br>
-                                                                 <p class="text-muted">{Relationship}</p>
-                                                             </p></div>
-
-                                                         </div>
-
-
+                                                           <h4 class="card-title">Guarantor</h4>
+                                                           <p class="card-text">Integration in progress</p>
+                                                           <a href="#" class="btn btn-primary">Go somewhere</a>
                                                        </div>
                                                    </div>
 
@@ -354,7 +350,156 @@
                        </div>
                    </div>
                </div>
+               <div class="tab-pane fade" id="v-pills-payments" role="tabpanel" aria-labelledby="v-pills-payments-tab">
+                   <div class="row">
+                       <div class="col-md-12">
+                           <div class="card no-b">
+                               <div class="card-header white b-0 p-3">
+                                   <h4 class="card-title">Loans</h4>
+                                   <small class="card-subtitle mb-2 text-muted">List of Loans.</small>
+                               </div>
+                               <div class="collapse show" id="invoiceCard">
+                                   <div class="card-body p-0">
+                                       <div class="table-responsive">
+                                           <table id="recent-orders"
+                                                  class="table table-hover mb-0 ps-container ps-theme-default">
+                                               <thead class="bg-light">
+                                               <tr>
+                                                   <th>Loan ID</th>
+                                                   <th>Applicant Name</th>
+                                                   <th>Loan Status</th>
+                                                   <th>Requested Amount</th>
+                                                   <th>Action</th>
+                                               </tr>
+                                               </thead>
+                                               <tbody>
+											   <?php
+											   foreach($loans as $ln):
+											   ?>
+                                               <tr>
+                                                   <td>BYSHESLB/B<?=$ln['batch']?>/<?=$ln['loan_id']?></td>
+                                                   <td><?=$profile['firstname']?> <?=$profile['lastname']?></td>
+                                                   <td><span class="badge badge-success"><?=$ln['status']?></span></td>
+                                                   <td>N <?=$ln['loan_amount']?></td>
+                                                   <td><a href="<?=base_url()?>admin/#" class="btn btn-xs btn-success">Approve Loan</a> <a href="<?=base_url()?>admin/#" class="btn btn-xs btn-danger">Reject Loan</a> <a href="<?=base_url()?>admin/#" class="btn btn-xs btn-warning">Withdraw Loan</a></td>
+                                               </tr>
+											   <?php
+											   endforeach;
+											   ?>
+                                               <!--<tr>
+                                                   <td>BYSHESLB/B2/124</td>
+                                                   <td>Ebimobowei Okpongu</td>
+                                                   <td><span class="badge badge-warning">Awaiting Approval</span>
+                                                   </td>
+                                                   <td>N300,000.00</td> 
+                                               </tr>-->
+                                               </tbody>
+                                           </table>
+                                       </div>
+                                   </div>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
 
+               </div>
+               <div class="tab-pane fade" id="v-pills-timeline" role="tabpanel" aria-labelledby="v-pills-timeline-tab">
+                   <div class="row">
+                       <div class="col-md-12">
+                           <!-- The time line -->
+                           <ul class="timeline">
+                               <!-- timeline time label -->
+                               <li class="time-label">
+                  <span class="badge badge-danger r-3">
+                    10 Feb. 2014
+                  </span>
+                               </li>
+                               <!-- /.timeline-label -->
+                               <!-- timeline item -->
+                               <li>
+                                   <i class="ion icon-envelope bg-primary"></i>
+                                   <div class="timeline-item card">
+                                       <div class="card-header white"><a href="#">BYSHESLB/B2/124	</a> Pending Approval   <span class="time float-right"><i class="ion icon-clock-o"></i> 12:05</span></div>
+                                       <div class="card-body">
+                                           Notes on loan
+                                       </div>
+                                       <div class="card-footer">
+                                           <a class="btn btn-primary btn-xs">View Loan</a>
+                                       </div>
+                                   </div>
+                               </li>
+                               <li>
+                                   <i class="ion icon-envelope bg-primary"></i>
+                                   <div class="timeline-item card">
+                                       <div class="card-header white"><a href="#">BYSHESLB/B2/101	</a> Loan Aproved   <span class="time float-right"><i class="ion icon-clock-o"></i> 12:05</span></div>
+                                       <div class="card-body">
+                                           Having met all required documentations, the applied loan has been approved for payment.
+                                       </div>
+                                       <div class="card-footer">
+                                           <a class="btn btn-primary btn-xs">View Loan</a>
+                                       </div>
+                                   </div>
+                               </li>
+
+
+                           </ul>
+                       </div>
+                       <!-- /.col -->
+                   </div>
+               </div>
+               <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
+                   <form class="form-horizontal">
+                       <div class="form-group">
+                           <label for="inputNamea" class="col-sm-2 control-label">Name</label>
+
+                           <div class="col-sm-10">
+                               <input class="form-control" id="inputNamea" placeholder="Name" type="email">
+                           </div>
+                       </div>
+                       <div class="form-group">
+                           <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+
+                           <div class="col-sm-10">
+                               <input class="form-control" id="inputEmail" placeholder="Email" type="email">
+                           </div>
+                       </div>
+                       <div class="form-group">
+                           <label for="inputName" class="col-sm-2 control-label">Name</label>
+
+                           <div class="col-sm-10">
+                               <input class="form-control" id="inputName" placeholder="Name" type="text">
+                           </div>
+                       </div>
+                       <div class="form-group">
+                           <label for="inputExperience" class="col-sm-2 control-label">Experience</label>
+
+                           <div class="col-sm-10">
+                               <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                           </div>
+                       </div>
+                       <div class="form-group">
+                           <label for="inputSkills" class="col-sm-2 control-label">Skills</label>
+
+                           <div class="col-sm-10">
+                               <input class="form-control" id="inputSkills" placeholder="Skills" type="text">
+                           </div>
+                       </div>
+                       <div class="form-group">
+                           <div class="col-sm-offset-2 col-sm-10">
+                               <div class="checkbox">
+                                   <label>
+                                       <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
+                                   </label>
+                               </div>
+                           </div>
+                       </div>
+                       <div class="form-group">
+                           <div class="col-sm-offset-2 col-sm-10">
+                               <button type="submit" class="btn btn-danger">Submit</button>
+                           </div>
+                       </div>
+                   </form>
+               </div>
            </div>
            </div>
         </div>
